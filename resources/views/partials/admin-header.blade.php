3f2f1c28e1afbc51cd0728be5c3bd492fee49ca7
sirap95 <gregoriosiravo@outlook.it>
2022-03-18 14:25:00 +0100

    bugfix

--- resources/views/partials/admin-header.blade.php
+++ resources/views/partials/admin-header.blade.php
@@ -3,7 +3,8 @@
         <a class="navbar-brand" href="{{ url('/') }}">
             Basic Blog
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -17,12 +18,13 @@
                 <!-- Dropdown -->
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                        Dropdown link
+                        Tags
                     </a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Link 1</a>
-                        <a class="dropdown-item" href="#">Link 2</a>
-                        <a class="dropdown-item" href="#">Link 3</a>
+                        <a class="dropdown-item" href="{{ route('guest.tag', ['id' => 1]) }}">Tag 1</a>
+                        <a class="dropdown-item" href="{{ route('guest.tag', ['id' => 2]) }}">Tag 2</a>
+                        <a class="dropdown-item" href="{{ route('guest.tag', ['id' => 3]) }}">Tag 3</a>
+                        <a class="dropdown-item" href="{{ route('guest.tag', ['id' => 4]) }}">Tag 4</a>
                     </div>
                 </li>
                 @if(\Illuminate\Support\Facades\Auth::check())
@@ -43,19 +45,20 @@
                     @endif
                 @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                           data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{route('admin.detail', ['id' => Auth::user()->id])}}"> View
+                                Profile
+                            </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
-                            <a class="dropdown-item" href="{{route('admin.detail', ['id' => Auth::user()->id])}}"> View Profile
-                            </a>
-
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
